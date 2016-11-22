f15a02935ae9010d8908c2cf84441b999fe99c6a
Elizenia Andrade <elizenia.liz@gmail.com>
2016-11-22 15:30:39 -0300

    Atualização do aplicativo telas endereços

---
+++ apps/Locaju/app/VidaOrganica/app/src/main/java/br/com/vidaorganica/vidaorganica/Cadastro.java
@@ -0,0 +1,16 @@
+package br.com.vidaorganica.vidaorganica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 83841048 on 18/10/2016.
+ */
+public class Cadastro extends AppCompatActivity
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.cadastro);
+        }
+}
--- apps/Locaju/app/VidaOrganica/app/src/main/java/br/com/vidaorganica/vidaorganica/MainActivity.java
+++ apps/Locaju/app/VidaOrganica/app/src/main/java/br/com/vidaorganica/vidaorganica/MainActivity.java
@@ -1,13 +1,28 @@
 package br.com.vidaorganica.vidaorganica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button cadastrar = (Button) findViewById(R.id.cadastro);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Button button = (Button)findViewById(R.id.cadastro);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Cadastro.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
